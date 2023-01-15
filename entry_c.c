c48f381a417ef6641c94f53b08ab0046938a6f00
Thomas Vogt <thomas@thomas-vogt.de>
2023-01-15 18:03:40 +0100

    Update Initialisation
    Zero BSS
    Copy Data Section from ROM to RAM

--- entry_c.c
+++ entry_c.c
@@ -10,7 +10,67 @@
  * @brief ARM Cortex-M4 C-Entry Point.
  **/
 
+#include <stdint.h>
+
+const char myStaticString[] = "MY_STATIC STRING";
+
+char myModString[] = "MY_STATIC STRING";
+
+char myNotInitData[255];
+
+/**
+ * @brief First C-Code startet from CPU initialisation.
+ **/
+void SystemInit( void ) __attribute__((used));
+
+/**
+ * @brief Zeros the BSS Section.
+ **/
+static void zeroBss( void );
+
+/**
+ * @brief Copies Data Section from Flash to RAM.
+ **/
+static void copyData( void );
+
 void SystemInit( void )
 {
+  zeroBss();
+  copyData();
+
   for (;;);
+}
+
+static void zeroBss( void )
+{
+  // Symbol is located on start of BSS Section
+  extern uint32_t __bss_start__;
+  // Symbol is located on end of BSS Section
+  extern uint32_t __bss_end__;
+
+  /* loop over BSS Section and set to Zero */
+  for ( uint32_t * bssPos = &__bss_start__; bssPos < &__bss_end__; ++bssPos )
+  {
+    *bssPos = 0U;
+  }
+}
+
+static void copyData( void )
+{
+  // Symbol is located on start of Data Section
+  extern uint32_t __data_start__;
+  // Symbol is located on end of Data Section
+  extern uint32_t __data_end__;
+  // Symbol is located on start of LMA Data Section located in Flash
+  extern const uint32_t __data_start_rom__;
+
+  // Source of Data (in Flash)
+  uint32_t const * dataCopyPos = &__data_start_rom__;
+
+  /* loop over Data Section and copy data from flash */
+  for ( uint32_t * dataPos = &__data_start__; dataPos < &__data_end__; ++dataPos )
+  {
+    *dataPos = *dataCopyPos;
+    ++dataCopyPos;
+  }
 }
